c8cd554dd0804c018f884c0f67a6ed0f6b0d9698
sathyan <sathyan576@gmail.com>
2020-07-09 12:08:38 +0530

    Toggle feature is added for choosing unit of temperature

--- app/src/main/java/com/example/weatherdetailer/SettingFragment.kt
+++ app/src/main/java/com/example/weatherdetailer/SettingFragment.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
 
 class SettingFragment : Fragment() {
+    private lateinit var switch:Switch
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,35 +23,25 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val nameTextView=view.findViewById<TextView>(R.id.usrnmeSettings)
-        val changeButton = view.findViewById<Button>(R.id.changeButton)
 
-        val  sharedPreferences= activity?.getSharedPreferences("weather", Context.MODE_PRIVATE)
 
-        val user: String? = sharedPreferences?.getString("name",null)
+        switch=view.findViewById(R.id.unit_switch)
 
-        nameTextView.text=user
-        var unit: String? = sharedPreferences?.getString("unit",null)
-        changeButton.text=unit
-        changeButton.setOnClickListener{
-            unit= sharedPreferences?.getString("unit",null)
-            if (unit=="celsius"){
-                var editor= sharedPreferences!!.edit()
-                editor.remove("unit")
-                editor.putString("unit","farenheit")
-                editor.commit()
-                changeButton.text="Farenheit"
 
-            }
-            else if (unit=="farenheit"){
-                var editor=sharedPreferences!!.edit()
-                editor.remove("unit")
-                editor.putString("unit","celsius")
-                editor.commit()
-                changeButton.text="Celsius"
-            }
-
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val m=if (isChecked) "farenheit" else "celsius"
+            switch.text=m
+            update(m)
         }
+
+    }
+
+    private fun update(unit:String){
+        val  sharedPreferences= activity?.getSharedPreferences("weather", Context.MODE_PRIVATE)
+        val editor= sharedPreferences!!.edit()
+        editor.remove("unit")
+        editor.putString("unit",unit)
+        editor.apply()
 
     }
 
